3c3f35b6db2705a3f8862977e7d59dcf049dab52
MaWeRo3214 <matthiaserselina@gmail.com>
2018-09-17 03:48:43 +0200

    De functie om een nieuwe woordenlijst aan te maken is volledig functioneel

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def touch(filename):
     filename.extend([".","w","d","l"])
     i = 0
     for value in filename:
-        if (value == " "):
+        if value == " ":
             filename[i] = "_"
         i = i + 1
     filename = "".join(filename)
@@ -26,7 +26,7 @@ def touch(filename):
         _ = system('type nul > ' + filename) 
  
     else: 
-        _ = system('touch' + filename)
+        _ = system('touch ' + filename)
 
 def ls(cd,cdabsolute):
     cdloop = pathlib.Path(cd)
@@ -39,21 +39,53 @@ def ls(cd,cdabsolute):
     for cf in cdloop.iterdir():
         paths_ls[cdabsolute].append(str(cf))
 
+def rm(filedirname):
+    if name == 'nt':
+        _ = system('del /F /Q ' + filedirname)
+        print("del " + filedirname)
+ 
+    else: 
+        _ = system('rm -rf ' + filedirname)
+        print("del " + filedirname)
+
 def loading():
     laadstr = "Loading"
     for number in range(0,8):
         clear()
         print(laadstr)
         laadstr = laadstr + "."
-        if (laadstr == "Loading...."):
+        if laadstr == "Loading....":
             laadstr = "Loading"
         sleep(0.25)
     sleep(0.5)
 
 def maak():
     clear()
-    filename = list(input("Welke bestandsnaam moet de nieuwe woordenlijst krijgen?\n"))
+    filename = list(input("Welke bestandsnaam moet de nieuwe woordenlijst krijgen?: "))
     touch(filename)
+    print("'/save' om de lijst op te slaan en te stoppen \n'/stop' om te stoppen")
+    filename = "".join(filename)
+    with open(filename, "w") as currentwdl:
+        while True:
+            woord = input("Voer een woord in: ")
+            if woord == "/save":
+                delete = False
+                break
+            elif woord == "/stop":
+                delete = True
+                break
+            currentwdl.write(woord + "\n")
+            ans = input("Voer de betekenis / vertaling van " + woord + " in: ")
+            if ans == "/save":
+                delete = False
+                break
+            elif ans == "/stop":
+                delete = True
+                break
+            currentwdl.write(ans + "\n")
+    if delete == True:
+        rm(filename)
+    menu()
     
 def verander():
     clear()
