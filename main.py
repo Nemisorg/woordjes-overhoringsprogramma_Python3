f1bc45abde950fdf3380c42be5e5f3d99e676ac3
MaWeRo3214 <matthiaserselina@gmail.com>
2018-09-28 04:05:14 +0200

    bestandsnaam bugs voor lijst maken en lijst verwijderen gefixt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from os import system, name
 import os
 from time import sleep
 import pathlib
+import re
 
 paths_ls = {}
 wdl_list = [".","w","d","l"]
@@ -13,27 +14,30 @@ def clear():
         _ = system('clear')
 
 def characterfix(filename):
-    i = 0
-    for value in filename:
-        if value == " ":
-            filename[i] = "_"
-        elif value == "<" or value == ">" or value == ":" or value == '"' or value == "/" or value == "\\" or value == "|" or value == "?" or value == "*":
-            filename[i] = ""
-        i += 1
-    filename = "".join(filename)
-
+    filename.replace(" ", "_")
+    while bool(re.match("^\w+$",filename)) == False:
+        clear()
+        filename = input("'" + filename + "' is geen correcte bestandsnaam. Voer een andere bestandsnaam in, '/cancel' om te annuleren: ")
+        if filename == "/cancel":
+            return filename
     return filename
 
 def confirmoverwrite(filename):
     if os.path.isfile(filename) == True:
-        overwrite = 0
+        overwrite = ""
         while overwrite not in ["A", "O"]:
-                overwrite = input("Het bestand dat u wilt maken bestaat al, \nwilt u het overschrijven (O) of een andere bestandsnaam kiezen (A)?: ")
-                overwrite = overwrite.title()
+            clear()
+            overwrite = input("Het bestand dat u wilt maken bestaat al, \nwilt u het overschrijven (O) of een andere bestandsnaam kiezen (A)? '/cancel' om te annuleren: ")
+            if overwrite == "/cancel":
+                return overwrite
+            overwrite = overwrite.title()
         while overwrite == "A" and os.path.isfile(filename) == True:
-            filename = list(input("Welke bestandsnaam moet de nieuwe woordenlijst krijgen?: "))
-            filename.extend([".","w","d","l"])
-            filename = "".join(filename)
+            clear()
+            filename = input("Welke bestandsnaam moet de nieuwe woordenlijst krijgen? '/cancel' om te annuleren: ")
+            if filename == "/cancel":
+                return filename
+            filename = characterfix(filename)
+            filename = filename + ".wdl"
 
     return filename
 
@@ -57,27 +61,25 @@ def cp(oldpath,newpath):
         _ = system('cp -r ' + oldpath + " " + newpath)
 
 def maak():
-    filename = list(input("Welke bestandsnaam moet de nieuwe woordenlijst krijgen? '/cancel' om te annuleren: "))
-    while len(filename) == 0:
-        clear()
-        filename = list(input("Welke bestandsnaam moet de nieuwe woordenlijst krijgen? '/cancel' om te annuleren: "))
-    filename.extend([".","w","d","l"])
-
-    if "".join(filename) == "/cancel.wdl":
-        return 0
-    
+    filename = ""
+    filename = input("Welke bestandsnaam moet de nieuwe woordenlijst krijgen? '/cancel' om te annuleren: ")
+    if filename == "/cancel":
+        return
     filename = characterfix(filename)
+    
+    if filename == "/cancel":
+        return
+    
+    filename = filename + ".wdl"
     filename = confirmoverwrite(filename)
+
+    if filename == "/cancel":
+        return
 
     cp(filename, filename + "cache")
     clear()
     
     print("'/save' om de lijst op te slaan en te stoppen \n'/stop' om te stoppen")
-    # open file
-    # woord = input("Voer een woord in: ")
-    # while woord != "stop"
-    #   doe je ding
-    #   woord = input("Voer een woord in: ")
     with open(filename, "w") as currentwdl:
         while True:
             woord = input("Voer een woord in: ")
@@ -118,8 +120,9 @@ def verwijder():
             clear()
             print("Geen bestanden om te verwijderen...")
             sleep(1)
-            return 0
+            return
 
+        clear()
         cancel = False
         delete = ""
         while delete not in paths_ls[os.getcwd()]:
@@ -128,14 +131,27 @@ def verwijder():
                 if filedirs.endswith(".wdl"):
                     print(filedirs[:-4])
             delete = input("Welk bestand had u gewenst te verwijderen? '/cancel' om te annuleren: ")
+
+            if delete == "/cancel":
+                return
+
+            delete = characterfix(delete)
+
+            if delete == "/cancel":
+                return
+
             delete = delete + ".wdl"
             clear()
-            if delete == "/cancel.wdl":
-                return 0
 
-        rm(delete)
-        print("Bestand succesvol verwijderd...")
-        sleep(1)
+        confirmdelete = "-"
+        while confirmdelete not in ["Y","N",""]:
+            confirmdelete = input("Weet u zeker dat u het bestand '" + delete[:-4] + "' wilt verwijderen? (y,N): ")
+            confirmdelete = confirmdelete.title()
+            clear()
+        if confirmdelete == "Y":
+            rm(delete)
+            print("Bestand succesvol verwijderd...")
+            sleep(1)
 
 
 def menu():
@@ -145,9 +161,6 @@ def menu():
         actie = input("Wat wilt u doen?:\nMaak een nieuwe woordenlijst (M)\nVerander een bestaande woordenlijst (V)\nVoeg woorden toe aan een bestaande woordenlijst (A)\nOverhoor een woordenlijst (O)\nVerwijder een woordenlijst (R)\nStop het programma (S)\n\n")
         actie = actie.title()
         clear()
-        # while actie != "S":
-        #   doe iets
-        #   actie = input("")
         if actie == "M":
             maak()
         #elif actie == "V":
